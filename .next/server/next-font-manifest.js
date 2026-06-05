bf3edae405bcfdd3989fa662430553465a169050
Bananaur <brozashannah25@gmail.com>
2026-06-06 00:49:07 +0800

    2

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/Bananaur/jake-gehan-wedding/app/page\":[\"static/media/c27321a0deeb793e-s.p.woff2\"],\"/home/Bananaur/jake-gehan-wedding/app/layout\":[\"static/media/6c596dfcddeca1e9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"J76FsrbAKQNVymPsFVYUBP8u75YqUet0zRa1vb7WZXM=\"\n}"
